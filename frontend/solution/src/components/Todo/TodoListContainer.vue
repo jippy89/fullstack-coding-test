7fd8d2e6dbfbf056d30f0ad217d206b064ba3f50
Jip Irfandy <irfandyjip98@gmail.com>
2019-08-31 06:28:35 +0700

    create 'TodoListContainer.vue'

---
+++ frontend/solution/src/components/Todo/TodoListContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="todo in todoList" :key="todo.id">
+      {{ todo.text }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    todoList: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
