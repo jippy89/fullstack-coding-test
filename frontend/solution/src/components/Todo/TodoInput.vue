05dc835bb543c63fb5882ae057c22d3e54dbe0ec
Jip Irfandy <irfandyjip98@gmail.com>
2019-08-31 06:14:08 +0700

    create 'TodoInput.vue' which emits 'addTodo' on submit

---
+++ frontend/solution/src/components/Todo/TodoInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="todo">
+    <button type="submit">Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      todo: ''
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      this.$emit('addTodo', this.todo)
+    }
+  }
+}
+</script>
