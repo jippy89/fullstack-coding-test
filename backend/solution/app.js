9fe41eb215225576e12b8a6440283ab91b9f74f6
Jip Irfandy <irfandyjip98@gmail.com>
2019-08-31 20:55:22 +0700

    connect 'routes/todo.js' with 'app.js'

--- backend/solution/app.js
+++ backend/solution/app.js
@@ -3,9 +3,13 @@ const express = require('express'),
 
 const PORT = 3000
 
+// Routes
+const todoRoutes = require('./routes/todo')
+
 app.get('/', (req, res) => {
   res.send("Hola soy Express!")
 })
+app.use('/todo', todoRoutes)
   
 app.listen(PORT, () => {
   console.log(`Express server has started at localhost:${PORT}`)
