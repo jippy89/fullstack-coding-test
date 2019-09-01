556d30a2d51f4923aa228e6db42bd51eda34751d
Jip Irfandy <irfandyjip98@gmail.com>
2019-09-01 23:42:16 +0700

    add test for 'POST /todo'

--- backend/solution/tests/todo.js
+++ backend/solution/tests/todo.js
@@ -8,6 +8,91 @@ chai.use(chaiHttp);
 chai.should();
 
 describe("Todo", () => {
+  describe("POST /todo", () => {
+    it("should post a 'todo' object", (done) => {
+      chai.request(app)
+        .post('/todo')
+        .type('json')
+        .send({
+          "title": "Todo title",
+          "deadline": "2019-10-01"
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('id');
+          res.body.id.should.equal(1);
+          res.body.should.have.property('title');
+          res.body.title.should.equal("Todo title");
+          res.body.should.have.property('done_flag');
+          res.body.done_flag.should.equal("false");
+          res.body.should.have.property('deadline');
+          res.body.deadline.should.equal("2019-10-01");
+          done()
+        })
+    })
+
+    it("should REJECT a post without 'title' property", (done) => {
+      chai.request(app)
+        .post('/todo')
+        .type('json')
+        .send({
+          "deadline": "2019-10-01"
+        })
+        .end((err, res) => {
+          res.should.have.status(422);
+          res.body.should.have.property('errors')
+          done()
+        })
+    })
+
+    it("should REJECT a post if 'title' property is NOT a string", (done) => {
+      chai.request(app)
+        .post('/todo')
+        .type('json')
+        .send({
+          "title": 1,
+          "deadline": "2019-10-01"
+        })
+        .end((err, res) => {
+          res.should.have.status(422);
+          res.body.should.have.property('errors')
+          res.body['errors'][0].msg.should.equal("Must be a 'string' type")
+          done()
+        })
+    })
+
+    it("should REJECT a post without a 'deadline' property", (done) => {
+      chai.request(app)
+        .post('/todo')
+        .type('json')
+        .send({
+          "title": "Doin' some work"
+        })
+        .end((err, res) => {
+          res.should.have.status(422)
+          res.body.should.have.property('errors')
+          done()
+        })
+    })
+
+    it("should REJECT a post if a 'deadline' property is NOT in 'yyyy-mm-dd' format", (done) => {
+      chai.request(app)
+        .post('/todo')
+        .type('json')
+        .send({
+          "title": "Doin' some work",
+          "deadline": "10-11-2019"
+        })
+        .end((err, res) => {
+          res.should.have.status(422)
+          res.body.should.have.property('errors')
+          res.body['errors'][0].msg.should.equal("Must be in 'yyyy-mm-dd' format")          
+          done()
+        })
+    })
+  })
+
   describe("GET /todo", () => {
     // Test to get all todos'
     it("should get all todos", (done) => {
